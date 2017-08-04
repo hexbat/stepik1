d34c76d286a5224fa74f39342599ddb79175d90c
hexbat <hexbat@gmail.com>
2017-08-04 10:46:23 +0300

    content-Type

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 def wsgi_application(environ, start_response):
   status = '200 OK'
-  headers = [('Content-Type', 'text/plain')]
   qs = environ['QUERY_STRING']
   if '?' in qs:
     qs = qs.split('?')[1]
   body = '\n'.join(qs.split('&'))
+  headers = [('Content-type','text/plain'),
+        ('Content-Length', str(len(body))]         
   start_response(status, headers)
   return [body]
