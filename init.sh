5f84ecd99edd9be33615bd40b4bb001ce6986246
hexbat <hexbat@gmail.com>
2017-08-04 15:27:14 +0300

    init_ask2

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
+sudo rm /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/ask
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/guni_ask.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
