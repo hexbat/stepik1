571f15633fc0557dc573d9ca59aa5344c6002629
hexbat <hexbat@gmail.com>
2017-08-04 22:46:05 +0300

    ask

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^6v4=4y+96%n1q(dv*y&pj_a7k6*ybz#l_xmdtp-n$7hmj*_19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
