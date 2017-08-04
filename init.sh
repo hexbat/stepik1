0a7d8ababd5dc3b94f4ad3867c36fd31ceb89713
hexbat <hexbat@gmail.com>
2017-08-04 22:51:50 +0300

    guni

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
+sudo apt-get install python-django
+
 sudo rm /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo rm /etc/gunicorn.d/test
-sudo rm /etc/gunicorn.d/ask
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo rm /etc/gunicorn.d/ask
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/guni_ask.conf   /etc/gunicorn.d/ask
+sudo gunicorn -c /home/box/web/etc/guni_ask.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 
