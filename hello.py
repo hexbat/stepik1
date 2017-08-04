cda76c8e88b4e916c5174d67296a5d264c25f720
hexbat <hexbat@gmail.com>
2017-08-04 10:24:32 +0300

    hello.py init

---
+++ hello.py
@@ -0,0 +1,9 @@
+def wsfi_application(environ, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  qs = environ['QUERY_STRING']
+  if '?' in qs:
+    qs = qs.split('?')[1]
+  body = '\n'.join(qs.split('&'))
+  start_response(status, headers)
+  return [body]
