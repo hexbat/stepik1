52b9b2b1ab7b6a79316ee4582e262f7ab1e709e7
hexbat <hexbat@gmail.com>
2017-08-21 16:54:02 +0300

    qa

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,23 +1,42 @@
-class User(models.Model):
-    name = models.CharField(max_length=24)
-    friends = models.ManyToManyField('User')
-    groups = models.ManyToManyField('Group')
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Group(models.Model):
-    name = models.CharField(max_length=100)
-    moderator = models.ForeignKey('User')
-    
+from django.db import models
+# Create your models here.
+from django.contrib.auth.models import User
 
-    Question - вопрос
-title - заголовок вопроса
-text - полный текст вопроса
-added_at - дата добавления вопроса
-rating - рейтинг вопроса (число)
-author - автор вопроса
-likes - список пользователей, поставивших "лайк"
 
-Answer - ответ
-text - текст ответа
-added_at - дата добавления ответа
-question - вопрос, к которому относится ответ
-author - автор ответа
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='%(class)s_author')
+    likes = models.ManyToManyField(User, related_name='%(class)s_likes')
+
+    def QuestionMnager(self):
+        return Question.objects.all()
+
+    def new(self):
+        return Question.objects.order_by('added_at').desc()
+
+    def popular(self):
+        return Question.objects.order_by('rating').desc()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class Likes(models.Model):
+    question = models.ForeignKey(
+        Question,
+        related_name="like_question"
+    )
+    user = models.ForeignKey(
+        User,
+        related_name="like_user"
+    )
