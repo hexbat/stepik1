f956f2d6ad393825641cbf90ce1b28b611fa9536
ovandr12 <hexbat@gmail.com>
2017-08-22 09:13:01 +0300

    init

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@ sudo rm /etc/gunicorn.d/test
 sudo rm /etc/gunicorn.d/ask
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/guni_ask.conf   /etc/gunicorn.d/ask
-sudo gunicorn -c /home/box/web/etc/guni_ask.conf ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/guni_ask.conf ask.wsgi:application
+cd ~/web/ask
+gunicorn --bind=0.0.0.0:8000 ask.wsgi:application
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
 
