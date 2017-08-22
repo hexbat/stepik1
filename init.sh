9cde886d95c916c59e2821c33c8572a408d69288
ovandr12 <hexbat@gmail.com>
2017-08-22 09:42:42 +0300

    __init__

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@ sudo rm /etc/gunicorn.d/ask
 sudo ln -s /home/box/web/etc/guni_ask.conf   /etc/gunicorn.d/ask
 #sudo gunicorn -c /home/box/web/etc/guni_ask.conf ask.wsgi:application
 cd ~/web/ask
-gunicorn --bind=0.0.0.0:8000 ask.wsgi:application
+#gunicorn --bind=0.0.0.0:8000 ask.wsgi:application
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
+chmod +x manage.py
 
