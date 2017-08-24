f879133446f0e6f47c9df1ea01238b3a7beead79
ovandr12 <hexbat@gmail.com>
2017-08-24 15:16:18 +0300

    templates

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # url(r'.*', views.test),
     url(r'^$', views.index, name='index'),
     url(r'^/popular/.*$', views.popular, name='popular'),
-    url(r'^/question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
 ]
