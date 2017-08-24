383aebf18f0bf58130fb00035b34b6c19cd51d5c
ovandr12 <hexbat@gmail.com>
2017-08-24 15:36:53 +0300

    templates

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from qa import views
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
+    url(r'.*', include('qa.urls')),
     # url(r'^login/$', include('qa.urls')),
     # url(r'^signup/$', include('qa.urls')),
-    url(r'^question/\d+/$', include('qa.urls')),
+    # url(r'^question/\d+/', include('qa.urls')),
     # url(r'^ask/$', include('qa.urls')),
     # url(r'^popular/$', include('qa.urls')),
     # url(r'^new/$', include('qa.urls')),
