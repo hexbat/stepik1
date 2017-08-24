f1132ad91336fd429b19639e75363221d6277877
ovandr12 <hexbat@gmail.com>
2017-08-24 16:28:25 +0300

    templates

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from qa import views
 
 urlpatterns = [
+    url(r'^question/', include('qa.urls')),
     url(r'.*', include('qa.urls')),
     # url(r'^login/$', include('qa.urls')),
     # url(r'^signup/$', include('qa.urls')),
-    url(r'^question/', include('qa.urls')),
+
     # url(r'^ask/$', include('qa.urls')),
     # url(r'^popular/$', include('qa.urls')),
     # url(r'^new/$', include('qa.urls')),
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # url(r'.*', views.test),
-    url(r'^$', views.test, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.test, name='question'),
     # url(r'^popular/.*$', views.popular, name='popular'),
 
