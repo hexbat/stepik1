dd82dbedec7171b16f58225f16f03445610d380d
ovandr12 <hexbat@gmail.com>
2017-08-24 15:24:28 +0300

    templates

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from qa import views
 
 urlpatterns = [
     url(r'^$', include('qa.urls')),
@@ -22,7 +23,7 @@ urlpatterns = [
     # url(r'^signup/$', include('qa.urls')),
     url(r'^question/\d+/$', include('qa.urls')),
     # url(r'^ask/$', include('qa.urls')),
-    url(r'^popular/$', include('qa.urls')),
+    # url(r'^popular/$', include('qa.urls')),
     # url(r'^new/$', include('qa.urls')),
     # url(r'^new/$', include('qa.urls')),
 ]
